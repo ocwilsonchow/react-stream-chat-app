9c4bda4a36dc86ed7f7f1d65de690eef783aa3ce
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-03-07 17:52:11 +0800

    adding team channel preview

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -3,15 +3,34 @@ import { Avatar, useChatContext } from 'stream-chat-react'
 
 const TeamChannelPreview = ({ channel, type}) => {
   const { channel: activeChannel, client } = useChatContext()
+
   const ChannelPreview = () => (
     <div>
       # {channel?.data?.name || channel?.data?.id}
     </div>
   )
 
-  return (
-    <div>
+  const DirectPreview = () => {
+    const members = Object.values(channel.state.members).filter(({ user}) => user.id !== client.userID)
 
+    return (
+      <div>
+       <Avatar
+         image={members[0]?.Avatar.user?.image}
+         name={members[0]?.Avatar.user?.fullName}
+         size={24}
+       />
+       <div>{members[0]?.Avatar.user?.fullName}</div>
+      </div>
+    )
+  }
+
+  // 44:40
+  return (
+    <div onClick={() => {
+      console.log(channel)
+    }}>
+        {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
     </div>
   )
 }
