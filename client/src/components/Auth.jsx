3f661d1611d2f6c1eb1f4656bcd4ac122773eff9
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-03-08 11:35:26 +0800

    updated auth forms

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -11,46 +11,108 @@ import {
   FormErrorMessage,
   FormHelperText,
   Input,
+  Button,
 } from '@chakra-ui/react';
 
 const Auth = () => {
   const [isSignup, setIsSignup] = useState(true);
 
   const handleChange = () => {};
+  const switchMode = () => {
+    setIsSignup(prevIsSignup => !prevIsSignup);
+  };
 
   return (
     <Flex justifyContent="center">
       <Box p={2} minW="500px">
-        <Text fontWeight='bold'>{isSignup ? 'Sign Up' : 'Sign In'}</Text>
+        <Text fontWeight="bold" fontSize="3xl">
+          {isSignup ? 'Sign Up' : 'Sign In'}
+        </Text>
         {isSignup && (
           <FormControl onSubmit={() => {}} isRequired>
-
-            <FormLabel my={2} htmlFor="email">
+            <FormLabel my={2} htmlFor="username">
               Username
             </FormLabel>
-            <Input id="username" type="text" />
-
+            <Input
+              id="usernameInput"
+              name="username"
+              type="text"
+              onChange={() => console.log('typing')}
+            />
             <FormLabel my={2} htmlFor="email">
               Email address
             </FormLabel>
-            <Input id="email" type="email" />
-
-            <FormLabel my={2} htmlFor="email">
+            <Input
+              id="emailInput"
+              name="email"
+              type="email"
+              onChange={() => console.log('typing')}
+            />
+            <FormLabel my={2} htmlFor="phone">
               Phone Number
             </FormLabel>
-            <Input id="email" type="email" />
-
-            <FormLabel my={2} htmlFor="email">
+            <Input
+              id="phoneInput"
+              name="phone"
+              type="number"
+              onChange={() => console.log('typing')}
+            />
+            <FormLabel my={2} htmlFor="password">
               Password
             </FormLabel>
-            <Input id="email" type="email" />
-
-            <FormLabel my={2} htmlFor="email">
+            <Input
+              id="passwordInput"
+              name="password"
+              type="password"
+              onChange={() => console.log('typing')}
+            />
+            <FormLabel my={2} htmlFor="passwordConfirmation">
               Password Confirmation
             </FormLabel>
-            <Input id="email" type="email" />
+            <Input
+              id="passwordConfirmationInput"
+              name="passwordConfirmation"
+              type="password"
+              onChange={() => console.log('typing')}
+            />
+            <Button colorScheme="blue" my={6}>
+              Sign up
+            </Button>
           </FormControl>
         )}
+
+        {!isSignup && (
+          <FormControl onSubmit={() => {}} isRequired>
+            <FormLabel my={2} htmlFor="email">
+              Email address
+            </FormLabel>
+            <Input
+              id="emailInput"
+              name="email"
+              type="email"
+              onChange={() => console.log('typing')}
+            />
+            <FormLabel my={2} htmlFor="password">
+              Password
+            </FormLabel>
+            <Input
+              id="passwordInput"
+              name="password"
+              type="password"
+              onChange={() => console.log('typing')}
+            />
+            <Button colorScheme="blue" my={6}>
+              Sign in
+            </Button>
+          </FormControl>
+        )}
+
+        <Center>
+          {isSignup ? 'Already have an account?' : "Don't have an account?"}
+          <Button size='xs' onClick={switchMode}>
+            {isSignup ? 'Sign In' : 'Sign Up'}
+          </Button>
+        </Center>
       </Box>
     </Flex>
   );
